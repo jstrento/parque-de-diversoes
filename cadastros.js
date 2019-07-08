526c5ee5cac4849bfde90fb18673d7757fa3e0e9
Antonio <antonio_s_goncalves@estudante.sc.senai.br>
2019-07-08 11:35:51 -0300

    começando js

---
+++ cadastros.js
@@ -0,0 +1,5 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var cadastros = JSON.parse(localStorage.getitem)
